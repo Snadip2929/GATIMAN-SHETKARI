8795c16e161f36fea0e11ccab910ce32cc701c6a
Jayesh Nikam <nikamjayesh20@gmail.com>
2026-08-09 19:30:02 +0530

    operation

---
+++ ATMOperation.java
@@ -0,0 +1,8 @@
+public class ATMOperation {
+    public static void main(String[] args) {
+        int balance = 6400;
+        int note = 500;
+        int noofNotes = balance/note;
+        System.out.println(noofNotes);
+    }
+}
