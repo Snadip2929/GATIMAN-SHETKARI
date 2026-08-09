d1a63e15cb97d2b0518d7f1118ad7ef314ad4e8e
Jayesh Nikam <nikamjayesh20@gmail.com>
2026-08-09 19:52:54 +0530

    addition

---
+++ divide.java
@@ -0,0 +1,12 @@
+public class divide
+{
+    public static void main(String[]args)
+    {
+        int aaccept=500;
+        int baccept=250;
+        System.out.println(aaccept);
+         System.out.println(baccept);
+         int add = aaccept+baccept;
+         System.out.println(add);
+    }
+}
